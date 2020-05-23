54f20590272247335fa447e4ebb5db97b7897f6a
react ninja <aadhar54@gmail.com>
2020-05-23 23:10:48 +0530

    working on posting comments part

--- src/public/app/my-posts.js
+++ src/public/app/my-posts.js
@@ -1,3 +1,4 @@
+
 function displayMyPosts(){
     const {id} = JSON.parse(window.localStorage.user)
     
@@ -14,7 +15,7 @@ function displayMyPosts(){
                 <p class="card-text">${post.body.substr(0,200)}
                 <a href="#">read more...</a>
                 </p>
-                <a href="#" class="a-c-tag" data-toggle="modal" data-target="#exampleModal"  class="card-link" data-component="${postid}">Comment</a>
+                <a href="#" class="c-link" data-toggle="modal" data-target="#exampleModal" data-component=${postid}>Comment</a>
                 <a href="#" class="card-link">Like</a>
               </div>
             </div>
@@ -24,18 +25,29 @@ function displayMyPosts(){
     })
   }
 
+function onLinkClick(){
+
+  //Notes for the doubt mentor
+
+  //Made this function so that it runs after loadmyposts() and things are done sequentially rather than asynchronosly.
+  //But still enable to set up this click event for the comment link .I thought when I will click on comment , we can get the ev.taget and using that and the attr jquery for data-component we can get the post id .
+  
+  $('.c-link').click(function(ev){
+  console.log(1)
+  console.log(ev.target)
+  })
+  console.log($('.c-link'))
+  
+}
+
 
 function postComm(){
 
-    let title = $('#c-t').val();
-    let body = $('#c-b').val();
+    const title = $('#c-t').val()
+    const body = $('#c-b').val()
     const userId =JSON.parse(window.localStorage.user).id
-    const list = $('.a-c-tag')
-    //const index = list.index($(ev.target));
-    //const postId = $('.a-c-tag').attr("data-component")
-    console.log('title',title)
-    console.log('body',body)
-    console.log('userId',userId)
+    
     //console.log('postId',postId)
-    //$.post('api/comments',{userId,},())
+    
 }
+
